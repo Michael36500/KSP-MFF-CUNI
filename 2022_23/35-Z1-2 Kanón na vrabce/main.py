40bdde0214e16fa7fa2c1f31a4638fea33ba5a83
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-07-12 19:08:31 +0200

    more improvement

--- 2022_23/35-Z1-2 Kanón na vrabce/main.py
+++ 2022_23/35-Z1-2 Kanón na vrabce/main.py
@@ -1,5 +1,26 @@
+import numpy as np
 # 1. array o velikosti zahrady
-# 2. loop 
-#     a) do A B dám čís. kanónu, jedu úhlopříčky
-#     b) pokud je kanón na nenule, tak výsledek
-#     c) pokud tam něco je, tak skip
+x = 6
+y = 6
+
+zahrada = np.zeros((x, y))
+zahrada -= 1
+# print(zahrada)
+
+kanony = [[1, 2], [4, 5], [2, 3], [4, 3]]
+
+for lpnb in range(len(kanony)):
+
+    # a) do A B dám čís. kanónu, jedu úhlopříčky
+    a = kanony [lpnb] [1]
+    b = kanony [lpnb] [0]
+
+    print(a, b)
+    zahrada[a][b] = lpnb
+
+zahrada = np.flip(zahrada, 0)
+print(zahrada)
+
+for lpnb in range(len(kanony)):
+    kanon = kanony[lpnb]
+    
