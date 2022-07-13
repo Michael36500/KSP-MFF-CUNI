b5894258e3b88432f7905381929c53ff49153536
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-07-13 21:51:17 +0200

    více komentáře

--- 2022_23/35-Z1-2 Kanón na vrabce/main.py
+++ 2022_23/35-Z1-2 Kanón na vrabce/main.py
@@ -12,23 +12,27 @@ zahrada -= 1
 # import pole s pozicemi kanónů
 kanony = [[1, 2], [4, 5], [2, 3], [4, 3]]
 
+# loop co dává kanóny do pole / zahrady
 for lpnb in range(len(kanony)):
 
-    # a) do A B dám čís. kanónu, jedu úhlopříčky
+    # do A a B loadnu pozici kanónu
     a = kanony [lpnb] [1]
     b = kanony [lpnb] [0]
 
-    # print(a, b)
+    # na dané pozice umístím kanón
     zahrada[a][b] = lpnb
 
-# zahrada = np.flip(zahrada, 0)
-# print(zahrada)
-
+# loop checkující jestli na úhlopříčce není už nějakej, potřeba dodělat
 for lpnb in range(len(kanony)):
+    # získám X a Y kanónu
     kanon = kanony[lpnb]
     print(kanon)
+    
+    # najdu začáteční Y pozici (přes X se loopuju)
     start_y = kanon[1] - kanon[0]
     print(start_y)
+
+    # loop pro jednu úhlopříčku
     for a in range(x):
         if a < 0:
             continue
