f0508521dea2bbf027b75eb2df86ef46bf3233b9
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-07-15 21:40:52 +0200

    maybe something?

--- 2022_23/35-Z1-2 Kanón na vrabce/main.py
+++ 2022_23/35-Z1-2 Kanón na vrabce/main.py
@@ -22,12 +22,13 @@ for lpnb in range(len(kanony)):
     # na dané pozice umístím kanón
     zahrada[a][b] = lpnb
 
+out = []
+
 # loop checkující jestli na úhlopříčce není už nějakej, potřeba dodělat
 for lpnb in range(len(kanony)):
     print(zahrada)
     # získám X a Y kanónu
     kanon = kanony[lpnb]
-    # print(kanon)
     
     # najdu začáteční Y pozici (přes X se loopuju)
     start_y = kanon[1] - kanon[0]
@@ -35,13 +36,36 @@ for lpnb in range(len(kanony)):
     act_y = start_y
     # loop pro jednu úhlopříčku
     # range x === loop přes jednotlivé sloupce
+    print()
+    closest_before = None
+    closest_after = None
+    after_thisone = False
+    tempup = []
     for sloupec in range(x):
+        tempdown = []
         if act_y < 0 or act_y >= x: #idk it just works
             pass
         else:
             # print(act_y, sloupec)
             print(zahrada[act_y, sloupec])
+            if zahrada[act_y, sloupec] == lpnb:
+                after_thisone = True
+                continue
 
+            if not after_thisone: # means when you are before canon that you are checking
+                closest_before = zahrada[act_y, sloupec]
+
+            if after_thisone: # means when you are after canon that you are checking
+                closest_after = zahrada[act_y, sloupec]
+
+        tempdown.append(closest_before)
+        tempdown.append(closest_after)
+        tempdown.append(lpnb)
+        tempdown.append("next one")
+
+        tempup.append(tempdown)
 
         act_y += 1
-    break
+    out.append(tempup)
+    # break
+print(out)
