17ee0ad10e4b27c9297181c40035c92a4620b39a
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-07-17 21:23:12 +0200

    final version (i believe)

--- 2022_23/35-Z1-2 Kanón na vrabce/main.py
+++ 2022_23/35-Z1-2 Kanón na vrabce/main.py
@@ -17,19 +17,60 @@ x = int(x_line) #výška
 
 y_line = ""
 for a in line:
+    line = line[1:]
     if a == " ":
         break
     y_line = str(y_line) + str(a)
 y = int(y_line)  #šířka
 
-print(x, y)
+kolikk = ""
+for a in line:
+    line = line[1:]
+    if a == " ":
+        break
+    kolikk = str(kolikk) + str(a)
+kolik = int(kolikk) 
+
+print(x, y, kolik)
 
 # generuju pole polí o velikosti zahrady
 zahrada = np.zeros((x, y))
 zahrada -= 1
 
 # import pole s pozicemi kanónů
-kanony = [[1, 2], [4, 5], [2, 3], [4, 3]]
+kanony = []
+def read_kanon():
+    global file
+    global kanony
+
+    line = file.readline()
+    x_line = ""
+    for a in line:
+        line = line[1:]
+        if a == " ":
+            break
+        x_line = str(x_line) + str(a)
+    x = int(x_line) #výška
+
+    y_line = ""
+    for a in line:
+        line = line[1:]
+        if a == " ":
+            break
+        y_line = str(y_line) + str(a)
+    y = int(y_line)  #šířka
+
+    temp = []
+    temp.append(x)
+    temp.append(y)
+    
+    kanony.append(temp)
+
+for _ in range(kolik):
+    read_kanon()
+
+print(kanony)
+
 
 # loop co dává kanóny do pole / zahrady
 for lpnb in range(len(kanony)):
